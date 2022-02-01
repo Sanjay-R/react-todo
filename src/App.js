f12a80e72638e074abf2b08caf531045b621e60a
srachwani <s.rachwani@student.tudelft.nl>
2022-02-01 20:06:51 +0100

    Introducing state for overlay

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import Overlay from './components/Overlay';
-import Backdrop from './components/Backdrop';
 import Task from './components/Task';
 
 function App() {
   return (
     <div>
       <h1>All Tasks</h1>
-      <Task title="first one"/>
-      <Task title="task 2"/>
-      <Task title="third"/>
-      <Overlay/>
-      <Backdrop/>
+      <Task title="first one" />
+      <Task title="task 2" />
+      <Task title="third" />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,24 @@
+import {useState} from 'react'; //cz its a 3rd party library 
+import Backdrop from './Backdrop';
+import Overlay from './Overlay';
+
 function Task(props) {
-    function deleteHandler() {
+
+    const [overlayOpen, setOverlayOpen] = useState(false); //https://reactjs.org/docs/hooks-state.html
+
+    function changeOverlayState() {
         console.log("clicked " + props.title);
+        setOverlayOpen(!overlayOpen);
     }
 
     return (
         < div className="task-card">
             <h2 className="task-title">{props.title}</h2>
             <div className="actions">
-                <button className="red-btn btn" onClick={deleteHandler}>Delete</button>
+                <button className="red-btn btn" onClick={changeOverlayState}>Delete</button>
             </div>
+            { overlayOpen ? <Overlay /> : null}
+            { overlayOpen ? <Backdrop /> : null}
         </div >
     );
 }
